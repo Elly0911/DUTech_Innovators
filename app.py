18d26efd4b83d1a0c788701de1420e039d074808
Elly0911 <elishaechetty@gmail.com>
2025-03-13 04:57:13 +0200

    Updated login/signup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import Flask, render_template, redirect, url_for, flash, request, session, get_flashed_messages
 from forms import LoginForm, SignupForm
 from config import Config
 from models import db, User
@@ -19,18 +19,26 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+
+    list(get_flashed_messages())
+    user_logged_in = session.get('user_id', None)
+    return render_template('home.html', user_logged_in=user_logged_in)
 
 @app.route('/auth/<string:form_type>', methods=['GET', 'POST'])
 def auth(form_type="login"):
     login_form = LoginForm()
     signup_form = SignupForm()
+    result_document_filename = None
+
+    list(get_flashed_messages())
 
     if login_form.validate_on_submit() and form_type == "login":
         email = login_form.email.data
         password = login_form.password.data
         user = User.query.filter_by(email=email).first()
-        if user and user.password == password:   
+        if user and user.password == password:
+            session['user_id'] = user.id  
+            session['user_name'] = user.name  
             flash("Login successful!")
             return redirect(url_for('home'))
         else:
@@ -38,10 +46,7 @@ def auth(form_type="login"):
 
     if signup_form.validate_on_submit() and form_type == "signup":
         role = signup_form.role.data
-
-        # Handle tutor-specific validation
         average_grade = None
-        result_document_filename = None
 
         if role == "tutor":
             if signup_form.average_grade.data < 75:
@@ -58,8 +63,9 @@ def auth(form_type="login"):
 
             file = signup_form.result_document.data
             if file:
-                result_document_filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-                file.save(result_document_filename)
+                result_document_filename = file.filename
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], result_document_filename)
+                file.save(file_path)
                 average_grade = signup_form.average_grade.data
 
         new_user = User(
@@ -68,7 +74,7 @@ def auth(form_type="login"):
             password=signup_form.password.data,
             role=role,
             average_grade=average_grade,
-            result_document=result_document_filename
+            result_document=result_document_filename  
         )
 
         db.session.add(new_user)
@@ -76,9 +82,18 @@ def auth(form_type="login"):
 
         flash(f"Signup successful for {signup_form.email.data}!")
         return redirect(url_for('auth', form_type="login"))
-    
+
     return render_template('auth.html', form_type=form_type, login_form=login_form, signup_form=signup_form)
 
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    session.pop('user_name', None)
+    flash("You have been logged out!")
+    
+    list(get_flashed_messages())  
 
-if __name__ =='__main__':
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -2,6 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField, SubmitField, BooleanField, FloatField, FileField
 from wtforms.validators import DataRequired, Email, Length,  Optional, NumberRange
 from flask_wtf.file import FileAllowed, FileRequired
+from wtforms import ValidationError
+
+def validate_email_domain(form, field):
+    if not field.data.endswith('@dut4life.ac.za'):
+        raise ValidationError('Email must be in the format of "@dut4life.ac.za"')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -10,7 +15,7 @@ class LoginForm(FlaskForm):
 
 class SignupForm(FlaskForm):
     name = StringField('Full Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email(), validate_email_domain])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=5)])
     role = SelectField('Role', choices=[('student', 'Student'), ('tutor', 'Tutor')], validators=[DataRequired()])
     average_grade = FloatField('Average Grade', validators=[Optional(), NumberRange(min=0, max=100)])
